73797946c0fb93e0d98afbd94f10e27c9d84db81
andreyshilov0 <an.schilow@yandex.ru>
2023-03-11 14:18:50 +0300

    Adding login page

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements} from 'react-router-dom';
+import Auth from './pages/Auth';
+import Favorite from './pages/Favorite';
+import Main from './pages/Main';
+import AddFavorite from './pages/AddFavorite';
+import Error from './pages/Error';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  {createGlobalStyle} from 'styled-components'
+
+
+const Global = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  
+}
+` 
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <>
+  
+  <Route path = '/' element={<Auth/>}/>
+  <Route path = '/Main' element={<Main/>}/>
+  <Route path = '/Favorite' element={<Favorite/>}/>
+  <Route path = '/AddFavorite' element={<AddFavorite/>}/>
+  <Route path='/*' element={<Error/>}/>
+  </>
+))
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <Global/>
+      <RouterProvider router={router} />
+      <App/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+
+    <h1 style={{textAlign:"center"}}>Главная страница</h1>
+    
+    </div>
+  )
+}
+
+export default Main 
